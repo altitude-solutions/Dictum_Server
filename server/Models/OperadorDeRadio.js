38bb12a8a3a0673e60caf7357464f3a3004dc0ad
XavierCont <javierconts3@live.com>
2019-12-26 17:31:26 -0400

    test operador de radio

--- server/Models/OperadorDeRadio.js
+++ server/Models/OperadorDeRadio.js
@@ -20,7 +20,7 @@ const { sql } = require('../config/sql');
 const { Vehiculo } = require('./Vehicle');
 const { Ruta } = require('./Ruta');
 const { Usuario } = require('./User');
-const { Personal } = require('./Personal');
+const { Personal, Supervisor } = require('./Personal');
 
 
 // ===============================================
@@ -59,7 +59,9 @@ RegistroDeHorarios.belongsTo(Personal, {
     foreignKey: 'conductor'
 });
 
-
+RegistroDeHorarios.belongsTo(Usuario, {
+    foreignKey: 'usuario'
+});
 
 CicloDeHorarios.init({
     id: {
@@ -113,7 +115,172 @@ RegistroDeHorarios.hasMany(CicloDeHorarios, {
 });
 
 
+// ===============================================
+// Registro De Penalidades models
+// ===============================================
+class RegistroDePenalidades extends Model {};
+class RegistroDeDatos_OR extends Model {};
+class ListaDeDatos_OR extends Model {};
+RegistroDePenalidades.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    item: {
+        type: DataTypes.STRING
+    },
+    tipoDePenalidad: {
+        type: DataTypes.STRING
+    },
+    detalle: {
+        type: DataTypes.STRING(1023)
+    },
+    comentarios: {
+        type: DataTypes.STRING(1023)
+    },
+    sigma: {
+        type: DataTypes.STRING
+    },
+    horaDeRecepcion: {
+        type: DataTypes.BIGINT
+    },
+    horaDeRespuesta: {
+        type: DataTypes.BIGINT
+    },
+    respuesta: {
+        type: DataTypes.STRING(1023)
+    },
+    horaDeContrarespuesta: {
+        type: DataTypes.BIGINT
+    },
+    contrarespuesta: {
+        type: DataTypes.STRING(1023)
+    }
+}, {
+    sequelize: sql,
+    timestamps: false,
+    modelName: 'RegistroDePenalidades',
+    tableName: 'RegistroDePenalidades'
+});
+
+RegistroDePenalidades.belongsTo(Ruta, {
+    foreignKey: 'ruta'
+});
+
+RegistroDePenalidades.belongsTo(Vehiculo, {
+    foreignKey: 'movil'
+});
+
+RegistroDePenalidades.belongsTo(Supervisor, {
+    foreignKey: 'supervisor'
+});
+
+RegistroDePenalidades.belongsTo(Usuario, {
+    foreignKey: 'usuario'
+});
+
+ListaDeDatos_OR.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    dato: {
+        type: DataTypes.STRING
+    }
+}, {
+    sequelize: sql,
+    timestamps: false,
+    modelName: 'ListaDeDatos_OR',
+    tableName: 'ListaDeDatos_OR'
+});
+
+RegistroDeDatos_OR.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    idDiario: {
+        type: DataTypes.INTEGER
+    },
+    sigmaDeRecepcion: {
+        type: DataTypes.STRING
+    },
+    horaDeRecepcion: {
+        type: DataTypes.BIGINT
+    },
+    zona: {
+        type: DataTypes.STRING(1023)
+    },
+    direccion: {
+        type: DataTypes.STRING(1023)
+    },
+    cantidadPoda: {
+        type: DataTypes.STRING
+    },
+    detalle: {
+        type: DataTypes.STRING
+    },
+    comentarios: {
+        type: DataTypes.STRING
+    },
+    tipoDeContenedor: {
+        type: DataTypes.STRING
+    },
+    codigoDeContenedor: {
+        type: DataTypes.STRING
+    },
+    Mantenimiento: {
+        type: DataTypes.STRING
+    },
+    horaComunicacion: {
+        type: DataTypes.BIGINT
+    },
+    horaEjecucion: {
+        type: DataTypes.BIGINT
+    },
+    horaVerificacion: {
+        type: DataTypes.BIGINT
+    },
+    horaConciliacion: {
+        type: DataTypes.BIGINT
+    },
+    sigmaDeConciliacion: {
+        type: DataTypes.STRING
+    }
+}, {
+    sequelize: sql,
+    timestamps: false,
+    modelName: 'RegistroDeDatos_OR',
+    tableName: 'RegistroDeDatos_OR'
+});
+
+RegistroDeDatos_OR.belongsTo(ListaDeDatos_OR, {
+    foreignKey: 'dato'
+});
+
+RegistroDeDatos_OR.belongsTo(Personal, {
+    foreignKey: 'responsableComunicacion'
+});
+
+RegistroDeDatos_OR.belongsTo(Personal, {
+    foreignKey: 'responsableEjecucion'
+});
+
+RegistroDeDatos_OR.belongsTo(Supervisor, {
+    foreignKey: 'supervisor'
+});
+
+
 module.exports = {
     RegistroDeHorarios,
-    CicloDeHorarios
+    CicloDeHorarios,
+    RegistroDePenalidades,
+    RegistroDeDatos_OR,
+    ListaDeDatos_OR
 }
--- server/routes/operadorRadio.js
+++ server/routes/operadorRadio.js
@@ -21,7 +21,39 @@ const { verifyToken } = require('../middlewares/authentication');
 // ===============================================
 // Operador de radio related models
 // ===============================================
-const { RegistroDeHorarios, CicloDeHorarios } = require('../Models/OperadorDeRadio');
+const { RegistroDeHorarios, CicloDeHorarios, RegistroDeDatos_OR, RegistroDePenalidades, ListaDeDatos_OR } = require('../Models/OperadorDeRadio');
+
+
+// ===============================================
+// Get lista de datos
+// ===============================================
+app.get('/listaDeDatos_OR', verifyToken, (req, res) => {
+    let offset = req.query.from || 0;
+    let limit = req.query.to || 1000;
+    let user = req.user;
+    if (user.permisos.includes('or_leer')) {
+        ListaDeDatos_OR.findAndCountAll({
+            offset,
+            limit
+        }).then(reply => {
+            res.json({
+                listaDeDatos: reply.rows,
+                count: reply.count
+            });
+        }).catch(err => {
+            res.status(500).json({
+                err
+            });
+        });
+    } else {
+        res.status(403).json({
+            err: {
+                message: 'Acceso denegado'
+            }
+        });
+    }
+});
+
 
 // ===============================================
 // Create Penalty
@@ -31,9 +63,16 @@ app.post('/penalties', verifyToken, (req, res) => {
     let user = req.user;
     if (user.permisos.includes('or_escribir')) {
         console.log(body);
-        res.json({
-            body
-        });
+        RegistroDePenalidades.create(body)
+            .then(saved => {
+                res.json({
+                    penalidad: saved
+                });
+            }).catch(err => {
+                res.status(500).json({
+                    err
+                });
+            });
     } else {
         res.status(403).json({
             err: {
@@ -50,9 +89,35 @@ app.post('/registroDeHorarios', verifyToken, (req, res) => {
     let body = req.body;
     let user = req.user;
     if (user.permisos.includes('or_escribir')) {
+        // console.log(body);
         res.json({
-            body
+            ok: true
         });
+
+        console.log(new Date(Number(body[0].horarios[0].salida_base)).getTime());
+        console.log(new Date(Number(body[0].horarios[0].salida_base)));
+        for (let i = 0; i < body.length; i++) {
+            for (let j = 0; j < body[i].horarios.length; j++) {
+                let element = body[i].horarios[j];
+                let keys = Object.keys(element);
+                console.log(keys);
+                for (let k = 0; keys.length; k++) {
+                    if (element[keys[k]] == 14400000)
+                        element[keys[k]] = null;
+                }
+                console.log(element);
+            }
+        }
+        // RegistroDeHorarios.create(body)
+        //     .then(saved => {
+        //         res.json({
+        //             horarios: saved
+        //         });
+        //     }).catch(err => {
+        //         res.status(500).json({
+        //             err
+        //         });
+        //     });
     } else {
         res.status(403).json({
             err: {
@@ -62,5 +127,30 @@ app.post('/registroDeHorarios', verifyToken, (req, res) => {
     }
 });
 
+// ===============================================
+// Create registro de datos
+// ===============================================
+app.post('/ragistroDeDatos', verifyToken, (req, res) => {
+    let body = req.body;
+    let user = req.user;
+    if (user.permisos.includes('or_escribir')) {
+        RegistroDeDatos_OR.create(body)
+            .then(saved => {
+                res.json({
+                    horarios: saved
+                });
+            }).catch(err => {
+                res.status(500).json({
+                    err
+                });
+            });
+    } else {
+        res.status(403).json({
+            err: {
+                message: 'Acceso denegado'
+            }
+        });
+    }
+});
 
 module.exports = app;

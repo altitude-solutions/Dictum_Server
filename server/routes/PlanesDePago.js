e0105a63e8d777bf93ffcccdb8ebe79cd3bc3ae3
Javier Contreras <javier.contreras@altitudesolutions.org>
2020-02-26 09:26:12 -0400

    validate linea de credito

--- server/routes/PlanesDePago.js
+++ server/routes/PlanesDePago.js
@@ -11,6 +11,8 @@
 
 const express = require('express');
 const app = express();
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 
 // ===============================================
 // Middlewares
@@ -31,57 +33,57 @@ app.post('/planDePagos', verifyToken, (req, res) => {
         if (body.numeroDeContratoOperacion && body.monto && body.fechaFirma && body.moneda && body.plazo && body.frecuenciaDePagos && body.empresaGrupo && body.entidadFinanciera) {
             if (body.lineaDeCredito) {
                 LineasDeCredito.findByPk(body.lineaDeCredito)
-                .then(lineaDB => {
-                    PlanDePagos.findAll({
-                        where: {
-                            lineaDeCredito: body.lineaDeCredito
-                        }
-                    })
-                        .then(operaciones => {
-                            let lineaDeCredito_creditLimit = lineaDB.toJSON().monto;
+                    .then(lineaDB => {
+                        PlanDePagos.findAll({
+                            where: {
+                                lineaDeCredito: body.lineaDeCredito
+                            }
+                        })
+                            .then(operaciones => {
+                                let lineaDeCredito_creditLimit = lineaDB.toJSON().monto;
 
-                            operaciones.forEach(element => {
-                                let op_aux = element.toJSON();
-                                lineaDeCredito_creditLimit -= op_aux.monto;
-                            });
+                                operaciones.forEach(element => {
+                                    let op_aux = element.toJSON();
+                                    lineaDeCredito_creditLimit -= op_aux.monto;
+                                });
 
-                            if(lineaDeCredito_creditLimit >= body.monto) {
-                                if(lineaDB.fechaVencimiento >= body.fechaVencimiento) {
-                                    PlanDePagos.create(body)
-                                        .then(saved => {
-                                            res.json({
-                                                planDePagos: saved
+                                if (lineaDeCredito_creditLimit >= body.monto) {
+                                    if (lineaDB.fechaVencimiento >= body.fechaVencimiento) {
+                                        PlanDePagos.create(body)
+                                            .then(saved => {
+                                                res.json({
+                                                    planDePagos: saved
+                                                });
+                                            }).catch(err => {
+                                                res.status(500).json({
+                                                    err
+                                                });
                                             });
-                                        }).catch(err => {
-                                            res.status(500).json({
-                                                err
-                                            });
+                                    } else {
+                                        return res.status(400).json({
+                                            err: {
+                                                message: `La fecha de vencimiento de la línea de crédito es anterior que la finalización operación\nFecha de vencimiento: ${new Date(lineaDB.fechaVencimiento).getDate()}/${new Date(lineaDB.fechaVencimiento).getMonth() + 1}/${new Date(lineaDB.fechaVencimiento).getFullYear()}`
+                                            }
                                         });
-                                }else{
+                                    }
+                                } else {
                                     return res.status(400).json({
                                         err: {
-                                            message: `La fecha de vencimiento de la línea de crédito es anterior que la finalización operación\nFecha de vencimiento: ${new Date(lineaDB.fechaVencimiento).getDate()}/${new Date(lineaDB.fechaVencimiento).getMonth()+1}/${new Date(lineaDB.fechaVencimiento).getFullYear()}`
+                                            message: `El saldo de la línea de crédito es insuficiente para sustentar esta operación\nSaldo: ${lineaDeCredito_creditLimit} ${lineaDB.moneda}`
                                         }
                                     });
                                 }
-                            }else{
-                                return res.status(400).json({
-                                    err: {
-                                        message: `El saldo de la línea de crédito es insuficiente para sustentar esta operación\nSaldo: ${lineaDeCredito_creditLimit} ${lineaDB.moneda}`
-                                    }
+                            }).catch(err => {
+                                return res.status(500).json({
+                                    err
                                 });
-                            }
-                        }).catch(err => {
-                            return res.status(500).json({
-                                err
                             });
+                    })
+                    .catch(err => {
+                        return res.status(500).json({
+                            err
                         });
-                })
-                .catch(err => {
-                    return res.status(500).json({
-                        err
                     });
-                });
             } else {
                 PlanDePagos.create(body)
                     .then(saved => {
@@ -117,6 +119,129 @@ app.post('/planDePagos', verifyToken, (req, res) => {
         });
     }
 });
+
+app.get('/planDePagos', verifyToken, (req, res) => {
+    let user = req.user;
+
+    let whereGlobal = {};
+    let whereEntidades = {};
+    let whereEmpresas = {};
+    if (req.query.q != undefined) {
+        whereGlobal.numeroDeContratoOperacion = {
+            [Op.regexp]: req.query.q
+        };
+        // whereGlobal.tipoOperacion = {
+        //     [Op.regexp]: req.query.q
+        // };
+        // whereEntidades.nombreEntidad = {
+        //     [Op.regexp]: req.query.q
+        // };
+        // whereEmpresas.empresa = {
+        //     [Op.regexp]: req.query.q
+        // };
+    }
+
+    if (user.permisos.includes('fin_leer')) {
+        PlanDePagos.findAll({
+            where: whereGlobal,
+            include: [{
+                model: LineasDeCredito,
+                required: false
+            }, {
+                model: EmpresaGrupo,
+                where: whereEmpresas,
+                required: true
+            }, {
+                model: EntidadFinanciera,
+                where: whereEntidades,
+                required: true
+            }]
+        })
+            .then(planesDePago => {
+                res.json({
+                    planesDePago
+                });
+            })
+            .catch(err => {
+                return res.status(500).json({
+                    err
+                });
+            });
+    } else {
+        res.status(403).json({
+            err: {
+                message: 'Acceso denegado'
+            }
+        });
+    }
+});
+
+
+app.get('/planDePagos/:id', verifyToken, (req, res) => {
+    let user = req.user;
+    let id = Number(req.params.id);
+
+    if (user.permisos.includes('fin_leer')) {
+        PlanDePagos.findByPk(id, {
+            include: [{
+                model: LineasDeCredito,
+                required: false
+            }, {
+                model: EmpresaGrupo,
+                required: true
+            }, {
+                model: EntidadFinanciera,
+                required: true,
+                include: [{
+                    model: TipoDeEntidad,
+                    required: true
+                }]
+            }]
+        })
+            .then(planDePagos => {
+                if(!planDePagos) {
+                    return res.status(404).json({
+                        err: {
+                            message: 'Plan de pagos no encontrado'
+                        }
+                    });
+                }
+                planDePagos = planDePagos.toJSON();
+                CuotaPlanDePagos.findAll({
+                    where: {
+                        parent: planDePagos.id
+                    }
+                })
+                .then(cuotasDB => {
+                    let cuotas = [];
+                    cuotasDB.forEach(element => {
+                        cuotas.push(element.toJSON());
+                    });
+                    planDePagos.cuotas = cuotas;
+                    res.json({
+                        planDePagos
+                    });
+                })
+                .catch(err => {
+                    return res.status(500).json({
+                        err
+                    });
+                });
+            })
+            .catch(err => {
+                return res.status(500).json({
+                    err
+                });
+            });
+    } else {
+        res.status(403).json({
+            err: {
+                message: 'Acceso denegado'
+            }
+        });
+    }
+});
+
 
 app.post('/cuotaPlanDePagos', verifyToken, (req, res) => {
     let body = req.body;
